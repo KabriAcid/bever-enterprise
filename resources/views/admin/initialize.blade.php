ea3d62ca02cde684b61b04c8b529ffe303c6f1fa
KabriAcid <kabriacid01@gmail.com>
2025-11-23 00:52:59 +0100

    migrate to a different setup

--- resources/views/admin/initialize.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-
-@section('title', 'Admin Initialize')
-
-@section('content')
-<h1>Welcome, {{ $admin->name ?? 'Admin' }}</h1>
-<p>Email: {{ $admin->email ?? 'N/A' }}</p>
-<!-- Add more admin info or page content as needed -->
-@endsection
--- resources/views/client/initialize.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.client')
-
-@section('title', 'Initialize')
-
-@section('content')
-<h1>Welcome, {{ $user->name }}</h1>
-<p>Email: {{ $user->email }}</p>
-<!-- Add more user info or page content as needed -->
-@endsection
